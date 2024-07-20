892912366cb21265ff079155f029a40906083e66
Bruno Dias <brunodias40@gmail.com>
2024-07-20 14:35:27 -0300

    feat: add CreatePlaceDto and UpdatePlaceDto classes

---
+++ src/places/dto/create-place.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreatePlaceDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  city: string;
+
+  @IsNotEmpty()
+  @IsString()
+  state: string;
+}
