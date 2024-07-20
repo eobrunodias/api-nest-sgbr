47600610ea25daf4681eff89f31d256a3107de7e
Bruno Dias <brunodias40@gmail.com>
2024-07-20 14:36:55 -0300

    feat: add AuthGuard for route authorization

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,49 @@
+import { Request } from "express";
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { IS_PUBLIC_KEY } from "src/decorators/is-public.decorator";
+import { JwtService } from "src/jwt/jwt.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return true;
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException("token not found");
+    }
+
+    try {
+      const payload = await this.jwtService.verify(token);
+      // string -> number
+      request.userId = parseInt(payload.sub);
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(" ") ?? [];
+    return type === "Bearer" ? token : undefined;
+  }
+}
